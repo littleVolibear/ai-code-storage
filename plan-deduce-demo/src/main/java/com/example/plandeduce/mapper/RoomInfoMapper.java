f78c118a8442465fb3214aa31be7662560412a7c
liuyt <oftenten@gmail.com>
2026-05-22 00:15:45 +0800

    22

--- plan-deduce-demo/src/main/java/com/example/plandeduce/mapper/RoomInfoMapper.java
+++ plan-deduce-demo/src/main/java/com/example/plandeduce/mapper/RoomInfoMapper.java
@@ -2,6 +2,6 @@ package com.example.plandeduce.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.plandeduce.model.RoomInfo;
-
+//todo
 public interface RoomInfoMapper extends BaseMapper<RoomInfo> {
 }
